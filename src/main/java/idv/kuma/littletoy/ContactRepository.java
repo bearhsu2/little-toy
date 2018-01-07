b2c50689941d4f206f7450af75fa17aaea128528
KUMA-PC\bearhsu2 <bearhsu2@gmail.com>
2018-01-08 00:21:51 +0800

    add basic class and templates

---
+++ src/main/java/idv/kuma/littletoy/ContactRepository.java
@@ -0,0 +1,21 @@
+package idv.kuma.littletoy;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by bearhsu2 on 1/7/2018.
+ */
+@Repository
+public class ContactRepository {
+
+
+    public List<Contact> findAll(){
+        return null;
+    }
+
+    public void save(Contact contact){
+
+    }
+}
